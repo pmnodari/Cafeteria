65d29e47d32c03d8f00a94007d68d7b5863447df
pmnodari <pmnodari@gmail.com>
2022-10-22 20:18:58 -0300

    Se crea el Header html, y se configura gulp para el manejo de las Imagenes

--- gulpfile.js
+++ gulpfile.js
@@ -28,15 +28,25 @@ function css(done) {
     
     done();
 }
+//Manejo de las imagenes
+function imagenes() {
+    return src('src/img/**/*')
+            .pipe(dest('build/img'));
+
+    //El return reemplaza al done
+    /* Lo unico que hacemos es llevar las imagenes a otra carpeta: build. No especificamos el formato porque hay multiples. */
+}
 //Creamos el Watch
 function dev() {
     //watch('src/scss/app.scss',css);
     watch('src/scss/**/*.scss', css);
+    watch('src/img/**/*', imagenes);//Antes modificaciones de imagenes
     
 }
 exports.css=css;
 exports.dev=dev;
-exports.default=series(css, dev);
+exports.imagenes=imagenes;
+exports.default=series(imagenes, css, dev);
 
 //series-Se inicia una tarea, y hasta que finaliza, se inicia la siguiente.
 
